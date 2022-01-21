27a28eca57a2eb7ee4d0d068bb3bab1114ad2c6d
Sp00kyB00k <jelted@hotmail.nl>
2022-01-21 17:27:02 +0100

    initial commit

---
+++ src/ports.rs
@@ -0,0 +1,44 @@
+use crate::{
+    common_ports::MOST_COMMON_PORTS_100,
+    model::{Port, Subdomain},
+};
+
+use rayon::prelude::*;
+use std::net::{SocketAddr, ToSocketAddrs};
+use std::{net::TcpStream, time::Duration};
+
+pub fn scan_ports(mut subdomain: Subdomain) -> Subdomain {
+    subdomain.open_ports = MOST_COMMON_PORTS_100
+        .into_par_iter()
+        .map(|port| scan_port(&subdomain.domain, *port))
+        .filter(|port| port.is_open)
+        .collect();
+    subdomain
+}
+
+fn scan_port(hostname: &str, port: u16) -> Port {
+    let timeout = Duration::from_secs(3);
+    let socket_addressess: Vec<SocketAddr> = format!("{}:{}", hostname, port)
+        .to_socket_addrs()
+        .expect("port scanner: Creating socket address")
+        .collect();
+
+    if socket_addressess.len() == 0 {
+        return Port {
+            port,
+            is_open: false,
+        };
+    }
+
+    let is_open = if let Ok(_) = TcpStream::connect_timeout(&socket_addressess[0], timeout) {
+        true
+    } else {
+        false 
+    };
+
+    Port {
+        port,
+        is_open,
+    }
+}
+
